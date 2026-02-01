ac811e2f974c6d8433a7820f1f4563eb0f49154a
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-01 20:07:19 +0000

    updated jest biug and index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+import Ship from './models/ship.js';
+import Gameboard from './models/gameboard.js';
+import Player from './models/player.js';
+
+// Expose classes in console for testing purposes
+window.Ship = Ship;
+window.Gameboard = Gameboard;
+window.Player = Player;
--- src/models/gameboard.js
+++ src/models/gameboard.js
@@ -1,4 +1,4 @@
-import Ship from '../ship.js';
+import Ship from './ship.js';
 
 class Gameboard {
   constructor(size) {
