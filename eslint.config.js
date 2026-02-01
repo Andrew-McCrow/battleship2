32830a30b025019468fbe154f67216e9a393c50a
Andrew <131323494+Andrew-McCrow@users.noreply.github.com>
2026-02-01 17:44:30 +0000

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  { ignores: ['dist/**', 'node_modules/**'] },
+
+  // Base ESLint recommended rules
+  js.configs.recommended,
+
+  // Browser code (src files and eslint config itself)
+  {
+    files: ['src/**/*.{js,mjs}', 'eslint.config.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    rules: {
+      'no-var': 'error',
+      'prefer-const': 'error',
+      eqeqeq: ['error', 'always'],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+
+  // Node.js config files (webpack, prettier configs)
+  {
+    files: ['*.cjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+
+  // Prettier integration
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
